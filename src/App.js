45e689fd2d6ec67a3910131bb30d262a3bd48edb
Cyntia <cyntiasgimenez@gmail.com>
2024-02-26 22:01:04 +0100

    primera entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Button from 'react-bootstrap/Button';
+import CollapsibleExample from './components/navbar/NavBar.jsx'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <CollapsibleExample/> 
+        <ItemListContainer greeting="Bienvenidos a Pro-Shop Tennis"/> 
     </div>
   );
 }
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from '../cardWidget/CartWiget';
+
+function CollapsibleExample() {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark" collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Pro-Shop Tennis</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Ofertas</Nav.Link>
+            <Nav.Link href="#pricing">Destacados</Nav.Link>
+            <NavDropdown title="Productos" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Babolat</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Head
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Wilson</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <CartWidget counter = {10} />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleExample;
